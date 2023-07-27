e7d9f179b4f0bf59839c0b0f7a88e5f0c9f81ddc
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-07-27 17:30:02 +0300

    Add hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,11 +6,19 @@ import { FC } from "react";
 export const Hero: FC = () => {
   return (
     <section className={sass.hero}>
-      <Container>
-        <div className={sass.heroInner}>
-          
-        </div>
-      </Container>
+      <div className={sass.heroWrapper}>
+        <Container>
+          <div className={sass.heroInner}>
+            <p className={sass.heroText}>
+              <span>Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</span>
+            </p>
+            <div className={sass.heroTitleWrapper}>
+              <h1 className={sass.heroTitle}>mostly tastes with freshes</h1>
+              <button className={sass.heroTestBtn}>taste it</button>
+            </div>
+          </div>
+        </Container>
+      </div>
     </section>
   )
 }
